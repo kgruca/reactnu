30b5b6a6d0930c0aa10d3c45783b0fa95df93769
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-10 22:41:12 -0700

    added Header and Footer components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png'
 import CampsitesList from './features/campsites/CampsitesList';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <Navbar dark color='primary' sticky='top' expand='md'>
-                <Container>
-                    <NavbarBrand href='/'>
-                        <img src={NucampLogo} alt='Nucamp Logo' />
-                    </NavbarBrand>
-                </Container>
-            </Navbar>
+            <Header />
             <CampsitesList />
+            <Footer />
         </div>
     );
 }
