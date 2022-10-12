2ce03713d258cc4cae5970285a5d7358096eaf20
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-11 22:02:17 -0700

    created campsite directry page

---
+++ src/pages/CampsitesDirectoryPage.js
@@ -0,0 +1,25 @@
+import { Controller } from "react-spring";
+import { Container, Row, Col, Button } from "reactstrap";
+import CampsiteDetail from '../features/campsites/CampsiteDetail';
+import CampsitesList from "../features/campsites/CampsitesList";
+import { selectRandomCampsite } from "../features/campsites/campsitesSlice";
+
+
+const CampsiteDirectoryPage = () => {
+    const selectedCampsite = selectRandomCampsite();
+
+    return (
+        <Container>
+            <Row>
+                <Col sm='5' md='7'>
+                    <CampsitesList />
+                </Col>
+                <Col sm='7' md='5'>
+                    <CampsiteDetail campsite={selectedCampsite} />
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default CampsiteDirectoryPage;
