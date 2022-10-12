8f8cb30c4e00f0555de362efdea76eab62313978
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-11 21:54:17 -0700

    added campsites slice with a functon that returns all campsites and another that returns a random campsite

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,21 +1,24 @@
-import { CAMPSITES } from "../../app/shared/CAMPSITES";
 import {Col, Row} from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
+import { selectAllCampsites } from "./campsitesSlice";
 
 
 const CampsitesList = () => {
+    const campsites = selectAllCampsites();
+
     return (
-        <Row className="ms-auto">{CAMPSITES.map(campsite => {
-            return (
-                <Col 
-                    md='5'
-                    className="m-4"
-                    key={campsite.id}
-                >
-                    <CampsiteCard campsite={campsite} />
-                </Col>
-            );
-        })}
+        <Row className="ms-auto">
+            {campsites.map(campsite => {
+                return (
+                    <Col 
+                        md='5'
+                        className="m-4"
+                        key={campsite.id}
+                    >
+                        <CampsiteCard campsite={campsite} />
+                    </Col>
+                );
+            })}
         </Row>
     );
 };
---
+++ src/features/campsites/campsitesSlice.js
@@ -0,0 +1,10 @@
+import {CAMPSITES} from "../../app/shared/CAMPSITES";
+
+
+export const selectAllCampsites = () => {
+    return CAMPSITES;
+};
+
+export const selectRandomCampsite = () => {
+    return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
+};
