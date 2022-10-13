12b3be025d6cad079121df54dea14dbd52b711ca
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-12 22:13:22 -0700

    added navbar toggler to header nav elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,28 +18,31 @@ const Header = () => {
                 <img src={NucampLogo} alt='Nucamp Logo' className='float-start' />
                 <h1 className='mt-1'>NuCamp</h1>
             </NavbarBrand>
-            <Nav className='ms-auto' navbar>
-                <NavItem>
-                    <NavLink className='nav-link' to='/'>
-                        <i className='fa fa-home fa-lg' /> Home
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink className='nav-link' to='/directory'>
-                        <i className='fa fa-list fa-lg' /> Directory
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink className='nav-link' to='/about'>
-                        <i className='fa fa-info fa-lg' /> About
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink className='nav-link' to='/contact'>
-                        <i className='fa fa-address-card fa-lg' /> Contact
-                    </NavLink>
-                </NavItem>
-            </Nav>
+            <NavbarToggler />
+            <Collapse navbar>
+                <Nav className='ms-auto' navbar>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/'>
+                            <i className='fa fa-home fa-lg' /> Home
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/directory'>
+                            <i className='fa fa-list fa-lg' /> Directory
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/about'>
+                            <i className='fa fa-info fa-lg' /> About
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/contact'>
+                            <i className='fa fa-address-card fa-lg' /> Contact
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
         </Navbar>
     );
 };
