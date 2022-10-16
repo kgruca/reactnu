40d84e1f2dd1da5734679f2f1112671eaae02c98
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-15 21:44:05 -0700

    added route for campsite detail page to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import CampsiteDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
                     <Route path='/' element={<HomePage />} />
                     <Route path='contact' element={<ContactPage />} />
                     <Route path='directory' element={<CampsiteDirectoryPage />} />
+                    <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} /> 
             </Routes>
             <Footer />
         </div>
