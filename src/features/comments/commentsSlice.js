0b0bac4e77ee388404cda8aa42a82027accc58c5
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-15 23:12:13 -0700

    created comments slice

---
+++ src/features/comments/commentsSlice.js
@@ -0,0 +1,6 @@
+import { COMMENTS } from "../../app/shared/COMMENTS";
+
+export const selectCommentsByCampsiteId = campsiteId => {
+    return COMMENTS.filter(comment => comment.campsiteId === parseInt(campsiteId));
+};
+
