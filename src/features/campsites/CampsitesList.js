4c80a432b057a96f4bdf8f2b3c9faa56f28c6fba
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-15 22:08:40 -0700

    updated CampsitesList and CampsiteCard

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -3,7 +3,7 @@ import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from "./campsitesSlice";
 
 
-const CampsitesList = ({ setCampsiteId }) => {
+const CampsitesList = () => {
     const campsites = selectAllCampsites();
 
     return (
@@ -14,7 +14,6 @@ const CampsitesList = ({ setCampsiteId }) => {
                         md='5'
                         className="m-4"
                         key={campsite.id}
-                        onClick={() => setCampsiteId(campsite.id)}
                     >
                         <CampsiteCard campsite={campsite} />
                     </Col>
