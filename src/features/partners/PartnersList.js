4bd6a8fb00bebe7e92247105eb23bf937ec64149
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-16 20:17:51 -0700

    added PartnersList.js

--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -3,7 +3,19 @@ import Partner from "./Partner";
 import { selectAllPartners } from "./partnersSlice";
 
 const PartnersList = () => {
+    const partners = selectAllPartners();
 
+    return (
+        <Col className="mt-4">
+            {partners.map(partner => {
+                return (
+                    <div className="d-flex mb-5" key={partner.id}>
+                        <Partner partner={partner} />
+                    </div>
+                );
+            })}
+        </Col>
+    );
 };
 
-export default
+export default PartnersList;
