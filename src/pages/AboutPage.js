44796f3d0cbf618f2de6e731782ea9f6a4c6c440
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-16 20:19:55 -0700

    added PartnersList component to aboutpage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,5 +1,6 @@
 import { Col, Row, Container, Card, CardBody, CardHeader } from "reactstrap";
 import SubHeader from '../components/SubHeader';
+import PartnersList from "../features/partners/PartnersList";
 
 const AboutPage = () => {
     return (
@@ -62,6 +63,7 @@ const AboutPage = () => {
                 <Col xs='12'>
                     <h3>Community Partners</h3>
                 </Col>
+                <PartnersList />
             </Row>
         </Container>
     );
