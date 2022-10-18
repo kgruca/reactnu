acee6bdb9fd3f0821d93aef11f7c5dad4588ee61
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-17 20:06:29 -0700

    linked form validation to ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,6 @@
 import { Button, Label, Col, FormGroup } from "reactstrap";
-import { Formik, Field, Form } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { validateContactForm } from "../utils/validateContactForm";
 
 
 const ContactForm = () => {
@@ -22,6 +23,7 @@ const ContactForm = () => {
                 feedback: ''       
             }}
             onSubmit={handleSubmit}
+            validate={validateContactForm}
         >
             <Form>
                 <FormGroup row>
@@ -34,6 +36,9 @@ const ContactForm = () => {
                             placeholder='First Name'
                             className='form-control'
                         />
+                        <ErrorMessage name='firstName'>
+                            {msg => <p className='text-danger'>{msg}</p>}
+                        </ErrorMessage>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -46,6 +51,9 @@ const ContactForm = () => {
                             placeholder='Last Name'
                             className='form-control'
                         />
+                        <ErrorMessage name='lastName'>
+                            {msg => <p className='text-danger'>{msg}</p>}
+                        </ErrorMessage>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -58,6 +66,9 @@ const ContactForm = () => {
                             placeholder='Phone' 
                             className='form-control'
                         />
+                        <ErrorMessage name='phoneNum'>
+                            {msg => <p className='text-danger'>{msg}</p>}
+                        </ErrorMessage>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -71,6 +82,9 @@ const ContactForm = () => {
                             type='email'
                             className='form-control'
                         />
+                        <ErrorMessage name='email'>
+                            {msg => <p className='text-danger'>{msg}</p>}
+                        </ErrorMessage>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
