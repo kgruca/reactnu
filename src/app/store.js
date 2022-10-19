d929307942e9efecac35d1029c1b86f3c14845bd
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-19 15:47:56 -0700

    added a userSlice.js file with initialState for the use, createSlice(), and a user reducer

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,16 @@ import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import { userReducer } from '../features/user/userSlice';
+import logger from 'redux-logger';
 
 export const store = configureStore({
   reducer: {
     campsites: campsitesReducer,
     comments: commentsReducer,
     partners: partnersReducer,
-    promotions: promotionsReducer
+    promotions: promotionsReducer,
+    user: userReducer
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat([logger])
 });
