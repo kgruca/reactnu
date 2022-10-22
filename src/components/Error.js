8ba726b41bdd865adf9317e91b13b2c922ab02e2
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-22 14:25:33 -0700

    added an Error component

---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import { Col } from "reactstrap";
+
+
+const Error = ({ errMsg }) => {
+    return (
+        <Col>
+            <h4>{errMsg}</h4>
+        </Col>
+    );
+};
+
+export default Error;
