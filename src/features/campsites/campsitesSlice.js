975c9b636dde00ce33ec13cf4d5e9df9ad17587a
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-22 00:53:53 -0700

    async campsites

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,5 +1,4 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { act } from "react-dom/test-utils";
 // import {CAMPSITES} from "../../app/shared/oldData/CAMPSITES";
 import { baseUrl } from '../../app/shared/baseUrl';
 import { mapImageURL } from '../../utils/mapImageURL';
